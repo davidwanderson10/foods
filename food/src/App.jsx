5c68f4b538b28e626e6b2861626e797772281c0f
DAVID WANDERSON <profdavidwanderson@gmail.com>
2025-05-29 21:30:39 -0300

    criando o menu

--- food/src/App.jsx
+++ food/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
 import './App.css'
+import MenuPrime from './components/Menu'
 
 function App() {
 
   return (
     <>
+     <MenuPrime />
     </>
   )
 }
--- food/src/main.jsx
+++ food/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import 'primereact/resources/themes/bootstrap4-light-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
